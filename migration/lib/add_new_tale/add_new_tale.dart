c62336c26bd4efe669b4c8d7b2626a1929cef7f9
Andrii Antonov <pro100svitlo@gmail.com>
2022-03-30 21:52:35 +0200

    Add tale 210

--- migration/lib/add_new_tale/add_new_tale.dart
+++ migration/lib/add_new_tale/add_new_tale.dart
@@ -5,24 +5,32 @@ import 'package:migration/add_new_tale/audio_util.dart';
 import 'package:migration/migration/from_0_to_1/dto/_new/tale_dto.dart';
 
 void main() {
-  final createDateDelta = const Duration(days: 3).inMilliseconds;
-  const taleId = 209;
+  final createDateDelta = const Duration(days: 2).inMilliseconds;
+  const taleId = 210;
   final missing = _createDirectoriesIfMissing(taleId);
   if (missing) {
     print('directories was created 💪');
     print('now add some images and audio 😘');
     return;
   }
+
+  final crew = _getCrew();
+  final content = _getContent(taleId);
+  final tags = <String>{
+    if (content.first.text != null) Tags.text,
+    if (content.first.audio != null) Tags.audio,
+    if (crew?.authors?.isNotEmpty == true) Tags.author,
+    // Tags.lullaby,
+    Tags.poem,
+  };
+
   final tale = TaleDto(
     id: taleId,
-    name: 'Кіт в чоботях',
+    name: 'Зозуля й півень',
     createDate: DateTime.now().millisecondsSinceEpoch + createDateDelta,
-    tags: {
-      Tags.author,
-      Tags.audio,
-    },
-    content: _getContent(taleId),
-    crew: _getCrew(),
+    tags: tags,
+    content: content,
+    crew: crew,
   );
   final jsonMap = tale.toJson();
   final string = json.encode(jsonMap);
@@ -30,7 +38,7 @@ void main() {
 }
 
 TaleCrewDto? _getCrew() => TaleCrewDto(
-      authors: [77],
+      authors: [36],
       readers: null,
       musicians: null,
       translators: null,
@@ -52,6 +60,7 @@ ChapterAudioDto? _getAudio({
   required int taleId,
   required int chapterIndex,
 }) {
+  return null;
   final path = _getDirPath(taleId) + '$chapterIndex/audio.mp3';
   final data = getAudioData(path);
   return ChapterAudioDto(
@@ -73,7 +82,46 @@ bool _createDirectoriesIfMissing(int taleId) {
 
 String _getDirPath(int taleId) => '../data/2/tales/$taleId/';
 
-List<String>? _getText() => null;
+List<String>? _getText() => [
+      "— Як ти співаєш, Півне, веселенько...",
+      "— А ти, Зозуленько, ти, зіронько моя,",
+      "Виводиш гарно так і жалібненько,",
+      "Що іноді аж плачу я...",
+      "Як тільки що почнеш співати,",
+      "Не хочеться й пшениченьки клювати,-",
+      "Біжиш в садок мерщій...",
+      "— Тебе я слухала б довіку, куме мій,",
+      "Аби б хотів співати...",
+      "— А ти, голубонько, ти, кралечко моя,",
+      "Поки співаєш на калині,",
+      "То й весело мені, і забуваю я",
+      "Свою недоленьку, життя своє погане",
+      "Та безталанне...",
+      "А тільки замовчиш",
+      "Або куди летиш,-",
+      "Заниє серденько, неначе на чужині...",
+      "І їстоньки — не їм, і питоньки — не п'ю",
+      "Та виглядаю все Зозуленьку мою.",
+      "Як гляну на тебе — така ти невеличка,",
+      "Моя перепеличко,",
+      "А голосочок-то який!..",
+      "Тонесенький, милесенький такий...",
+      "Куди той соловей годиться?",
+      "— Спасибі, братику, за добреє слівце.",
+      "Як не кохать тебе за це?..",
+      "І ти виспівуєш, неначе та жар-птиця;",
+      "І далебі, що так,— пошлюся я на всіх.-",
+      "Де взявся Горобець, підслухав трохи їх",
+      "Та й каже: — Годі вам брехати",
+      "Та одно другого знічев'я вихваляти! —",
+      "Пурхнув — та й був такий.",
+      " ",
+      "За що ж,— хто-небудь попитає,-",
+      "Зозуля Півня вихваляє?",
+      "За те, що Півень годить їй",
+      "Та потакати добре вміє:",
+      "Рука, як кажуть, руку миє.",
+    ];
 
 class Tags {
   Tags._();
