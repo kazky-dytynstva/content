14539ecc7dc91459e3f064eb2de76d05dd5ecef0
Andrii Antonov <pro100svitlo@gmail.com>
2022-03-31 16:26:57 +0200

    Update from_2_to_2.dart migration: add tale creation section

--- migration/lib/migration/from_2_to_2/from_2_to_2.dart
+++ migration/lib/migration/from_2_to_2/from_2_to_2.dart
@@ -2,6 +2,13 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:migration/migration/from_0_to_1/dto/_new/person/person_dto.dart';
+import 'package:migration/migration/from_0_to_1/dto/_new/tale/chapter/audio/dto.dart';
+import 'package:migration/migration/from_0_to_1/dto/_new/tale/chapter/dto.dart';
+import 'package:migration/migration/from_0_to_1/dto/_new/tale/crew/dto.dart';
+import 'package:migration/migration/from_0_to_1/dto/_new/tale/dto.dart';
+import 'package:migration/utils/audio_util.dart';
+import 'package:migration/utils/tale_tags.dart';
+import 'package:migration/utils/to_json.dart';
 
 import '../base_migration.dart';
 
@@ -23,17 +30,27 @@ class From2to2 extends BaseDataMigration {
 
   @override
   Future<bool> migrate() async {
-    final addOk = await _AddPersonHelper(this).run();
-    return addOk;
+    final addPersonOk = true ?? (await _AddPersonHelper(this).run());
+    final addTaleOk = true ?? await _AddTaleHelper(this).run();
+    return addPersonOk && addTaleOk;
   }
 }
 
 abstract class _AddHelper {
   final From2to2 migration;
 
-  String get addItemName;
+  final String addItemName;
+  final String folderName;
 
-  _AddHelper(this.migration);
+  late int nextId;
+
+  _AddHelper(this.migration, this.addItemName, this.folderName);
+
+  final List<ToJson> originalList = [];
+
+  String get dataPath => '${migration.dataPathOld}/$folderName';
+
+  String get jsonPath => '$dataPath/list.json';
 
   Future<bool> run() async {
     migration.log('Add new $addItemName started ⏱');
@@ -60,45 +77,37 @@ abstract class _AddHelper {
 
   Future<bool> validate({bool post = false});
 
-  Future<void> revert();
+  Future<void> revert() async {
+    await saveJson(originalList);
+  }
+
+  Future<void> saveJson(List<ToJson> list) async {
+    list.sort((a, b) => b.id.compareTo(a.id));
+    final json = list.map((e) => e.toJson()).toList();
+    await migration.saveJsonListToFile(data: json, filePath: jsonPath);
+  }
 }
 
 class _AddPersonHelper extends _AddHelper {
-  _AddPersonHelper(From2to2 migration) : super(migration);
-
-  @override
-  String get addItemName => 'Person';
-
-  String get peopleDataPath => '${migration.dataPathOld}/people';
-
-  String get peopleJsonPath => '$peopleDataPath/list.json';
-
-  late int nextPersonId;
-
-  final List<PersonDto> originalPeople = [];
-
-  @override
-  Future<void> revert() async {
-    await savePeople(originalPeople);
-  }
+  _AddPersonHelper(From2to2 migration) : super(migration, 'Person', 'people');
 
   @override
   Future<bool> validate({
     bool post = false,
   }) async {
     try {
-      final people = await _getAllPeople();
+      final all = await _getAll();
 
-      final peopleIds = people.map((e) => e.id).toSet();
-      final peopleNames = people.map((e) => e.name).toSet();
+      final idList = all.map((e) => e.id).toSet();
+      final nameList = all.map((e) => e.name).toSet();
 
       assert(
-        peopleIds.length == peopleNames.length,
+        idList.length == nameList.length,
         'Looks like we have duplicate',
       );
 
       if (post) {
-        final path = '$peopleDataPath/img/$nextPersonId.jpg';
+        final path = '$dataPath/img/$nextId.jpg';
         final file = File(path);
         assert(
           file.existsSync(),
@@ -114,36 +123,198 @@ class _AddPersonHelper extends _AddHelper {
 
   @override
   Future<void> add() async {
-    final lastPersonId = await _getLastPersonId();
-    nextPersonId = lastPersonId + 1;
+    nextId = (await _getLastId()) + 1;
 
     final person = PersonDto(
-      id: nextPersonId,
+      id: nextId,
       name: 'Ніка Балаж',
       info:
           'Ілюстратор дитячого періодичного видання «Перший інтерактивний журнал для дітей Чарівний ліхтарик»',
     );
-    final allPeople = await _getAllPeople();
+    final allPeople = await _getAll();
     allPeople.add(person);
-    await savePeople(allPeople);
+    await saveJson(allPeople);
   }
 
-  Future<List<PersonDto>> _getAllPeople() async {
-    final json = await migration.readJsonList(peopleJsonPath);
+  Future<List<PersonDto>> _getAll() async {
+    final json = await migration.readJsonList(jsonPath);
     final people = json.map((e) => PersonDto.fromJson(e)).toList();
-    if (originalPeople.isEmpty) {
-      originalPeople.addAll(people);
+    if (originalList.isEmpty) {
+      originalList.addAll(people);
     }
     return people;
   }
 
-  Future<int> _getLastPersonId() async {
-    final people = (await _getAllPeople()).map((e) => e.id);
+  Future<int> _getLastId() async {
+    final people = (await _getAll()).map((e) => e.id);
+    return people.reduce(max);
+  }
+}
+
+class _AddTaleHelper extends _AddHelper {
+  _AddTaleHelper(From2to2 migration) : super(migration, 'Tale', 'tales');
+
+  late TaleDto tale;
+
+  String getTalePath(int chapterIndex) => '$dataPath/$nextId/$chapterIndex/';
+
+  @override
+  Future<bool> validate({bool post = false}) async {
+    try {
+      final all = await _getAll();
+
+      final idList = all.map((e) => e.id).toSet();
+      final nameList = all.map((e) => e.name).toSet();
+
+      assert(
+        idList.length == nameList.length,
+        'Looks like we have duplicate',
+      );
+
+      if (post) {
+        final chapterIndex = 0;
+        final path = getTalePath(chapterIndex);
+        final dir = Directory(path + 'img');
+        if (!dir.existsSync()) {
+          dir.createSync(recursive: true);
+          migration.log('Empty folder for tale was created');
+          throw Exception('Please add some content to the tale folder');
+        }
+
+        final imageFile = File(path + 'img/0.jpg');
+        assert(
+          imageFile.existsSync(),
+          'Image for the tale was not found',
+        );
+
+        if (tale.tags.contains(Tags.audio)) {
+          final audioFile = File(path + 'audio.mp3');
+          assert(
+            audioFile.existsSync(),
+            'Image for the tale was not found',
+          );
+        }
+      }
+      return true;
+    } catch (e) {
+      migration.log(e.toString());
+      return false;
+    }
+  }
+
+  Future<List<TaleDto>> _getAll() async {
+    final json = await migration.readJsonList(jsonPath);
+    final people = json.map((e) => TaleDto.fromJson(e)).toList();
+    if (originalList.isEmpty) {
+      originalList.addAll(people);
+    }
+    return people;
+  }
+
+  Future<int> _getLastId() async {
+    final people = (await _getAll()).map((e) => e.id);
     return people.reduce(max);
   }
 
-  Future<void> savePeople(List<PersonDto> people) async {
-    final json = people.map((e) => e.toJson()).toList();
-    await migration.saveJsonListToFile(data: json, filePath: peopleJsonPath);
+  @override
+  Future<void> add() async {
+    final createDateDelta = const Duration(days: 1).inMilliseconds;
+    nextId = (await _getLastId()) + 1;
+
+    final crew = _getCrew();
+
+    final content = _getContent();
+    final tags = <String>{
+      if (content.first.text != null) Tags.text,
+      if (content.first.audio != null) Tags.audio,
+      if (crew?.authors?.isNotEmpty == true) Tags.author,
+      // Tags.lullaby,
+      Tags.poem,
+    };
+
+    tale = TaleDto(
+      id: nextId,
+      name: 'Зозуля й півень',
+      createDate: DateTime.now().millisecondsSinceEpoch + createDateDelta,
+      tags: tags,
+      content: content,
+      crew: crew,
+    );
+
+    final all = await _getAll();
+    all.add(tale);
+    await saveJson(all);
   }
+
+  TaleCrewDto? _getCrew() => TaleCrewDto(
+        authors: [36],
+        readers: null,
+        musicians: null,
+        translators: null,
+        graphics: null,
+      );
+
+  List<TaleChapterDto> _getContent() => [
+        _createChapter0(),
+      ];
+
+  TaleChapterDto _createChapter0() => TaleChapterDto(
+        title: null,
+        imageCount: 1,
+        text: _getText(),
+        audio: _getAudio(chapterIndex: 0),
+      );
+
+  ChapterAudioDto? _getAudio({
+    required int chapterIndex,
+  }) {
+    return null;
+    final path = getTalePath(chapterIndex) + 'audio.mp3';
+    final data = getAudioData(path);
+    return ChapterAudioDto(
+      size: data.size,
+      duration: data.duration.inMilliseconds,
+    );
+  }
+
+  List<String>? _getText() => [
+        "— Як ти співаєш, Півне, веселенько...",
+        "— А ти, Зозуленько, ти, зіронько моя,",
+        "Виводиш гарно так і жалібненько,",
+        "Що іноді аж плачу я...",
+        "Як тільки що почнеш співати,",
+        "Не хочеться й пшениченьки клювати,-",
+        "Біжиш в садок мерщій...",
+        "— Тебе я слухала б довіку, куме мій,",
+        "Аби б хотів співати...",
+        "— А ти, голубонько, ти, кралечко моя,",
+        "Поки співаєш на калині,",
+        "То й весело мені, і забуваю я",
+        "Свою недоленьку, життя своє погане",
+        "Та безталанне...",
+        "А тільки замовчиш",
+        "Або куди летиш,-",
+        "Заниє серденько, неначе на чужині...",
+        "І їстоньки — не їм, і питоньки — не п'ю",
+        "Та виглядаю все Зозуленьку мою.",
+        "Як гляну на тебе — така ти невеличка,",
+        "Моя перепеличко,",
+        "А голосочок-то який!..",
+        "Тонесенький, милесенький такий...",
+        "Куди той соловей годиться?",
+        "— Спасибі, братику, за добреє слівце.",
+        "Як не кохать тебе за це?..",
+        "І ти виспівуєш, неначе та жар-птиця;",
+        "І далебі, що так,— пошлюся я на всіх.-",
+        "Де взявся Горобець, підслухав трохи їх",
+        "Та й каже: — Годі вам брехати",
+        "Та одно другого знічев'я вихваляти! —",
+        "Пурхнув — та й був такий.",
+        " ",
+        "За що ж,— хто-небудь попитає,-",
+        "Зозуля Півня вихваляє?",
+        "За те, що Півень годить їй",
+        "Та потакати добре вміє:",
+        "Рука, як кажуть, руку миє.",
+      ];
 }
