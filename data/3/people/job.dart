cafa4bf5ffe20e93cf3910a780c152ad0df253ea
Andrii Antonov <pro100svitlo@gmail.com>
2025-02-15 15:54:47 +0100

    fix

---
+++ data/3/people/job.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  final path =
+      File('/Users/andrii.antonov/dev/kazky/content/data/2/people/list.json');
+
+  final content = path.readAsStringSync();
+
+  final jsonContent = jsonDecode(content) as List<dynamic>;
+
+  jsonContent.sort((a, b) {
+    final aId = a['id'] as int;
+    final bId = b['id'] as int;
+
+    return aId.compareTo(bId);
+  });
+
+  final result = <Map<String, dynamic>>[];
+
+  var correction = 0;
+
+  for (var item in jsonContent) {
+    final person = item as Map<String, dynamic>;
+
+    final id = person['id'] as int;
+
+    if (id > 35) {
+      correction = 1;
+    }
+
+    final int correctId = id + correction;
+
+    final updatedPerson = {
+      'id': correctId,
+      'name': person['name'],
+      'surname': person['surname'],
+      'gender': person['gender'],
+      'url': person['url'],
+      'info': person['info'],
+      'roles': person['roles'],
+    };
+
+    result.add(updatedPerson);
+
+    if (id == 35) {
+      final updatedPerson = {
+        'id': correctId + 1,
+        'name': person['name'] + ' БРО',
+        'surname': person['surname'],
+        'gender': person['gender'],
+        'url': person['url'],
+        'info': person['info'],
+        'roles': person['roles'],
+      };
+      result.add(updatedPerson);
+    }
+  }
+
+  result.sort((a, b) {
+    final aId = a['id'] as int;
+    final bId = b['id'] as int;
+
+    return bId.compareTo(aId);
+  });
+
+  final updatedContent = jsonEncode(result);
+
+  path.writeAsStringSync(updatedContent);
+}
