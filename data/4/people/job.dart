80e651320c46191307a14466fb2bc8974312b4ce
Andrii Antonov <pro100svitlo@gmail.com>
2025-05-26 19:00:55 +0200

    Add new photos and update job script for people directory
    - Added original and thumbnail images for people 41 to 95.
    - Removed obsolete image file for person 78.
    - Updated job.dart script to handle file copying and renaming for new directory structure.
    - Improved logic to ensure original images are created from thumbnails when necessary.

--- data/4/people/job.dart
+++ data/4/people/job.dart
@@ -2,101 +2,60 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  final path = Directory(
-    '/Users/andrii.antonov/dev/kazky/content/data/3/people/img/',
+  final peopleDir = Directory(
+    '/Users/andrii.antonov/dev/kazky/content/data/4/people/',
   );
 
-  final files = path.listSync().whereType<File>().toList();
+  final dirs =
+      Directory('${peopleDir.path}')
+          .listSync()
+          .where((e) {
+            final fileName = e.path.split('/').last;
+            return int.tryParse(fileName) != null;
+          })
+          .cast<Directory>()
+          .toList();
 
-  files.sort((a, b) {
-    final aId = int.parse(a.path.split('/').last.split('.').first);
-    final bId = int.parse(b.path.split('/').last.split('.').first);
+  for (final dir in dirs) {
+    final files = dir.listSync().whereType<File>().toList();
+    if (files.length == 2) continue;
 
-    return bId.compareTo(aId);
-  });
+    final first = files.first;
 
-  for (final file in files) {
-    final filePath = file.path;
-    final fileName = filePath.split('/').last;
-    final id = int.parse(fileName.split('.').first);
-
-    if (id < 35) continue;
-
-    final incrementedId = id + 1;
-
-    final newFileName = fileName.replaceFirst('$id', '$incrementedId');
-    final newPath = filePath.replaceFirst('$fileName', '$newFileName');
-
-    if (id == 35) {
-      file.copySync(newPath);
-    } else {
-      file.renameSync(newPath);
-    }
+    final fileName = first.path.split('/').last;
+    final originalFileName = fileName.replaceAll('thumbnail', 'original');
+    final originalFile = File('${dir.path}/$originalFileName');
+    first.copySync(originalFile.path);
   }
+
+  // // files.sort((a, b) {
+  // //   final aId = int.parse(a.path.split('/').last.split('.').first);
+  // //   final bId = int.parse(b.path.split('/').last.split('.').first);
+
+  // //   return bId.compareTo(aId);
+  // // });
+
+  // for (final file in files) {
+  //   try {
+  //     final fileName = file.path.split('/').last;
+  //     final format = fileName.split('.').last;
+  //     final personId = int.parse(fileName.split('.').first);
+  //     late final String newPath;
+  //     if (fileName.contains('original')) {
+  //       newPath = '${peopleDir.path}$personId/photo.original.$format';
+  //     } else {
+  //       newPath = '${peopleDir.path}$personId/photo.thumbnail.$format';
+  //     }
+  //     final newFile = File(newPath);
+  //     if (!newFile.existsSync()) {
+  //       newFile.createSync(recursive: true);
+  //     }
+  //     file.copySync(newFile.path);
+  //     print('Copied ${file.path} to ${newFile.path}');
+  //   } catch (e) {
+  //     print('Error processing file ${file.path}: $e');
+  //   }
+  // }
+
+  print('All files processed successfully.');
 }
-//   final path =
-//       File('/Users/andrii.antonov/dev/kazky/content/data/2/people/list.json');
-
-//   final content = path.readAsStringSync();
-
-//   final jsonContent = jsonDecode(content) as List<dynamic>;
-
-//   jsonContent.sort((a, b) {
-//     final aId = a['id'] as int;
-//     final bId = b['id'] as int;
-
-//     return aId.compareTo(bId);
-//   });
-
-//   final result = <Map<String, dynamic>>[];
-
-//   var correction = 0;
-
-//   for (var item in jsonContent) {
-//     final person = item as Map<String, dynamic>;
-
-//     final id = person['id'] as int;
-
-//     if (id > 35) {
-//       correction = 1;
-//     }
-
-//     final int correctId = id + correction;
-
-//     final updatedPerson = {
-//       'id': correctId,
-//       'name': person['name'],
-//       'surname': person['surname'],
-//       'gender': person['gender'],
-//       'url': person['url'],
-//       'info': person['info'],
-//       'roles': person['roles'],
-//     };
-
-//     result.add(updatedPerson);
-
-//     if (id == 35) {
-//       final updatedPerson = {
-//         'id': correctId + 1,
-//         'name': person['name'] + ' БРО',
-//         'surname': person['surname'],
-//         'gender': person['gender'],
-//         'url': person['url'],
-//         'info': person['info'],
-//         'roles': person['roles'],
-//       };
-//       result.add(updatedPerson);
-//     }
-//   }
-
-//   result.sort((a, b) {
-//     final aId = a['id'] as int;
-//     final bId = b['id'] as int;
-
-//     return bId.compareTo(aId);
-//   });
-
-//   final updatedContent = jsonEncode(result);
-
-//   path.writeAsStringSync(updatedContent);
-// }
