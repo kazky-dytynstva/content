ab618d964311046a2cdc5bedf11a28810fdcf893
Andrii Antonov <pro100svitlo@gmail.com>
2025-11-28 21:48:44 +0100

    feat: update job processing to include additional fields and create_date in JSON

--- data/4/people/job.dart
+++ data/4/people/job.dart
@@ -6,27 +6,58 @@ void main() {
     '/Users/andrii.antonov/dev/kazky/content/data/4/people/',
   );
 
-  final dirs =
-      Directory('${peopleDir.path}')
-          .listSync()
-          .where((e) {
-            final fileName = e.path.split('/').last;
-            return int.tryParse(fileName) != null;
-          })
-          .cast<Directory>()
-          .toList();
+  final jsonFile = File('${peopleDir.path}list.json');
 
-  for (final dir in dirs) {
-    final files = dir.listSync().whereType<File>().toList();
-    if (files.length == 2) continue;
+  final jsonList = jsonDecode(jsonFile.readAsStringSync()) as List<dynamic>;
 
-    final first = files.first;
+  final now = DateTime.now();
 
-    final fileName = first.path.split('/').last;
-    final originalFileName = fileName.replaceAll('thumbnail', 'original');
-    final originalFile = File('${dir.path}/$originalFileName');
-    first.copySync(originalFile.path);
-  }
+  final people = jsonList.map((e) {
+    final map = {
+      'id': e['id'],
+      'name': e['name'],
+      'surname': e['surname']!.toString().trim(),
+      'gender': e['gender'],
+    };
+    final url = e['url'];
+    if (url != null) {
+      map['url'] = url;
+    }
+    final info = e['info'];
+    if (info != null) {
+      map['info'] = info;
+    }
+    final roles = e['roles'];
+    if (roles != null) {
+      map['roles'] = roles;
+    }
+    map['create_date'] = now.toIso8601String();
+    return map;
+  }).toList();
+
+  final updatedJson = jsonEncode(people);
+  jsonFile.writeAsStringSync(updatedJson);
+
+  // final dirs = Directory('${peopleDir.path}')
+  //     .listSync()
+  //     .where((e) {
+  //       final fileName = e.path.split('/').last;
+  //       return int.tryParse(fileName) != null;
+  //     })
+  //     .cast<Directory>()
+  //     .toList();
+
+  // for (final dir in dirs) {
+  //   final files = dir.listSync().whereType<File>().toList();
+  //   if (files.length == 2) continue;
+
+  //   final first = files.first;
+
+  //   final fileName = first.path.split('/').last;
+  //   final originalFileName = fileName.replaceAll('thumbnail', 'original');
+  //   final originalFile = File('${dir.path}/$originalFileName');
+  //   first.copySync(originalFile.path);
+  // }
 
   // // files.sort((a, b) {
   // //   final aId = int.parse(a.path.split('/').last.split('.').first);
