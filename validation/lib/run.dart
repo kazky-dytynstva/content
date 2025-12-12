33c1f6b7774197296ab1996157823c32af8263d7
Andrii Antonov <pro100svitlo@gmail.com>
2025-12-12 20:23:28 +0100

    feat: add original audio files generation in run.dart

--- validation/lib/run.dart
+++ validation/lib/run.dart
@@ -1,23 +1,42 @@
-import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  final file = File(
-    '/Users/andrii.antonov/dev/kazky/content/data/4/tales/list.json',
+  final dir = Directory(
+    '/Users/andrii.antonov/dev/kazky/content/data/4/tales/',
   );
-  final stringContent = file.readAsStringSync();
 
-  final list = jsonDecode(stringContent) as List<dynamic>;
+  final children = dir.listSync().whereType<Directory>().toList();
 
-  for (final item in list) {
-    final text = item['text'] as Map<String, dynamic>?;
-    if (text == null) continue;
+  for (final dir in children) {
+    final id = dir.path.split('/').last;
+    print('Processing $id');
+    if (id == '0') {
+      id.toString();
+    }
+    final audioDir = Directory('${dir.path}/audio/');
+    if (!audioDir.existsSync()) continue;
 
-    final paragraphs = text.remove('paragraphs');
-    text['items'] = paragraphs;
+    final files = audioDir.listSync().whereType<File>().toList();
+
+    final original = files.where((file) {
+      final name = file.path.split('/').last;
+      return !name.contains('audio.mp3');
+    }).firstOrNull;
+
+    final thumbnail = files.where((file) {
+      final name = file.path.split('/').last;
+      return name.contains('audio.mp3');
+    }).first;
+
+    if (original == null) {
+      final bytes = thumbnail.readAsBytesSync();
+
+      final thumbnailName = thumbnail.path.split('/').last;
+      final newName = thumbnailName.replaceFirst('audio.mp3', 'original.mp3');
+
+      final newFile = File('${audioDir.path}/$newName');
+      newFile.createSync();
+      newFile.writeAsBytesSync(bytes);
+    }
   }
-
-  final updatedContent = jsonEncode(list);
-
-  file.writeAsStringSync(updatedContent);
 }
